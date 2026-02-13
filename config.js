a727ae7dd02057ee1b1911f86e6806d88abe5b58
Zakaria_ALZ1 <zakaria.next.com@gmail.com>
2026-02-13 06:24:10 +0300

    FIX: Add missing config.js and script.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.ENV = { 
+  SUPABASE_URL: "https://deovtpdjugfkccnpxfsm.supabase.co", 
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlb3Z0cGRqdWdma2NjbnB4ZnNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4ODk1MDMsImV4cCI6MjA4NjQ2NTUwM30.gEoj1WhdkyOcJQ3bf66FbTxneKmPvqspSHzu3Rd-W8A", 
+  MAX_UPLOAD_SIZE: 2147483648, 
+  ALLOWED_FILE_TYPES: ['.zip', '.rar', '.7z', '.baldimod'] 
+}; 
